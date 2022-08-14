4133a524a08a36a7d95baf66e9bc414af9f0169a
Almont michel <michel.almont@gmail.com>
2022-08-14 13:53:51 -0400

    Updated Yarn lockfile

--- config/plugins.js
+++
@@ -1,7 +0,0 @@
-module.exports = ({ env }) => ({
-    "users-permissions": {
-      config: {
-        jwtSecret: env('CUSTOM_JWT_SECRET'),
-      }
-    }
-  })
--- config/server.js
+++ config/server.js
@@ -2,6 +2,6 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS',['OojYh6VINqZIPUuIj3te6g==', '8NJLhVV3J71OPJYrpz1E8A==', 'iXyrDNunNtrIHtjIS6dfVw==', 'kA5Xr1hhrXBUsCH3HOPD9A==']),
+    keys: env.array('APP_KEYS'),
   },
 });
